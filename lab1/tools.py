f7392afffbf3900c5d06bc8c5e507623127bfab4
hentci <1222bruce@gmail.com>
2024-07-11 14:14:43 +0800

    [add] tools

--- lab1/tools.py
+++ lab1/tools.py
@@ -25,3 +25,10 @@ def generate_XOR_easy():
         inputs.append([0.1*i, 1-0.1*i])
         labels.append(1)
     return np.array(inputs), np.array(labels).reshape(21, 1)
+
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+def derivative_sigmoid(x):
+    return np.multiply(x, 1.0 - x)
+
