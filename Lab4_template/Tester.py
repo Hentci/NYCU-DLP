c34bc2d057a8340a9a34c8f59bc3009450e8a1e6
hentci <1222bruce@gmail.com>
2024-08-14 17:54:11 +0800

    ..

--- Lab4_template/Tester.py
+++ Lab4_template/Tester.py
@@ -204,6 +204,9 @@ def main(args):
     model.load_checkpoint()
     model.eval()
 
+    pytorch_total_params = sum(p.numel() for p in model.parameters())
+
+    print(f"number of params: {pytorch_total_params}")
 
 
 
